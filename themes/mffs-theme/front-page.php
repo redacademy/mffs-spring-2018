18e874e830fafe63097b59b4a3531f3607454eca
Showwtime <jason.f.murphy@gmail.com>
2018-06-12 10:52:13 -0700

    deleted unneccessary php on front-page

--- themes/mffs-theme/front-page.php
+++ themes/mffs-theme/front-page.php
@@ -25,16 +25,10 @@ get_header(); ?>
 
 			<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
 		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
