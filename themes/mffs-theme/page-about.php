777ce15f2ee393d10022594f4551b487afcf18d4
Showwtime <jason.f.murphy@gmail.com>
2018-06-13 15:15:16 -0700

    added page-about.php

---
+++ themes/mffs-theme/page-about.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package mffs_theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			
+			<section class="about-carousel">
+			</section>
+			
+			<section class="about-do">
+			</section>
+			
+			<section class="about-values">
+			</section>
+			
+			<section class="about-team">
+			</section>
+			
+			<section class="about-awards">
+			</section>
+			
+			<section class="about-disclaimer">
+			</section>
+			
+			<section class="about-refuse">
+			</section>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
