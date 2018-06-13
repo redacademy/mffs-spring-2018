0b35a188a74d57c0cdf93fb18ac8ac3c9e147df4
johnstonstu <stu.w.johnston@gmail.com>
2018-06-13 11:23:04 -0700

    started styling footer

--- themes/mffs-theme/footer.php
+++ themes/mffs-theme/footer.php
@@ -11,7 +11,28 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					
+					    <p class="supporter-title">
+                        Become a Supporter 
+                        </p>
+                        <a href="#">Donate</a>
+                    </div>
+                    <div class="contact">
+                        <p class="contact-title">
+                        Contact us
+                        </p>
+                        <div class="tel-info">
+                            <i class="fas fa-phone"></i>
+                            <a href="tel:123456789">778-321-3054</a>
+                        </div>
+                        <div class="contact-address">
+                            <i class="fas fa-phone"></i>
+                            <p class="address"></p>
+                        </div>
+                    </div>
+                    <div class="news">
+                            <p class="news-title">Newsletter</p>
+                            <a href="#" class="news-subscribe">Click here to Subscribe!</a>
+                    </div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
