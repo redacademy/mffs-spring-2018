b329d75f8b1ca942d9a538e9830c9409a71bdc1d
johnstonstu <stu.w.johnston@gmail.com>
2018-06-14 12:05:50 -0700

    added elements to donate page

---
+++ themes/mffs-theme/page-donate.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package mffs_theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+            <!-- fisrt section donate testimonials -->
+            <section class="donate-testimonials">
+                <!-- TODO add testimonials img, text and link button -->
+            </section> <!-- end donate testimonials -->
+
+            <!-- second section donate button wth dropdown -->
+            <section class="donate-dropdown-button">
+                <h2 class="section-title">I want to give a helping hand to those in need of</h2>
+                <select>
+                    <option value="substance use counselling">Substance use counselling</option>
+                    <option value="mental health counselling">Mental health counselling</option>
+                    <option value="trauma counselling">Trauma counselling</option>
+                    <option value="grief counselling">Grief counselling</option>
+                    <option value="body image counselling">Body image counselling</option>
+                </select>
+                <button class="donate-button" type="button">Donate</button>
+            </section> <!-- end donate dropdown button -->
+
+            <!-- third section where donation money is going -->
+            <section class="donate-where">
+                <h2 class="sction-title-blue">Where is your money going?</h2>
+
+                <ul class="doate-where-grid">
+				<?php
+					$loop = CFS()->get( 'mffs_donation_reason_loop', 23 );
+						foreach ( $loop as $row ) { ?>
+						<li class="donate-where-single">
+                            <img src="<?php echo $row['mffs_donation_img_single'];?>" alt="">
+                            <h2 class="section-title"><?php echo $row['mffs_donation_single_title'];?></h2>
+							<P><?php echo $row['mffs_donation_single_text'];?></P>
+						</li>
+						<?php } ?>
+                </ul>
+
+                <!-- donate annual reports button -->
+                <a href="<?php echo CFS()->get( 'mffs_donation_annual_report', 23 ) ?>" download>View Annual Reports</a>
+            </section><!-- end donate where -->
+
+            <!-- fourth section donation help -->
+            <section class="donate-help">
+                <h2 class="sction-title-blue">How else can you help?</h2>
+                <h2 class="section-title"> <?php echo CFS()->get( 'mffs_donation_help_title', 23 ) ?> </h2>
+
+                <a href="<?php echo CFS()->get( 'mffs_donation_partner_url', 23 ) ?>" target="_blank">Link!
+                <img src="<?php echo CFS()->get( 'mffs_donation_partner_img', 23 ) ?>" alt=""/>
+                </a>
+                <p><?php echo CFS()->get( 'mffs_donation_partner_description_text', 23 ) ?></p>
+            </section><!-- end donate help -->
+
+            <!-- fifthe section donation info -->
+            <section class="donate-info">
+                <ul class="doate-info-grid">
+				<?php
+					$loop = CFS()->get( 'mffs_donation_info_loop', 23 );
+						foreach ( $loop as $row ) { ?>
+						<li class="donate-info-single">
+                            <h2 class="section-title"><?php echo $row['mffs_donation_info_title'];?></h2>
+							<P><?php echo $row['mffs_donation_info_text'];?></P>
+						</li>
+						<?php } ?>
+                </ul>
+            </section><!-- end donate info -->
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
