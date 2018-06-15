453a302717e57035d58403785e49d36714769790
johnstonstu <stu.w.johnston@gmail.com>
2018-06-15 11:17:47 -0700

    changed h2 classes on internship page

--- themes/mffs-theme/page-internship.php
+++ themes/mffs-theme/page-internship.php
@@ -12,21 +12,21 @@ get_header(); ?>
 
         <!-- first section internship title -->
             <section class="internship-title">
-                <h2 class="section-title"><?php echo CFS()->get( 'mffs_internship_title', 19 ) ?></h2>
+                <h2 class="title-no-bg"><?php echo CFS()->get( 'mffs_internship_title', 19 ) ?></h2>
                 <img src="<?php echo CFS()->get( 'mffs_internship_img', 19 ) ?>" alt="">
                 <p><?php echo CFS()->get( 'mffs_internship_text', 19 ) ?></p>
             </section> <!-- end internship title -->
 
         <!-- second section internship contact -->
             <section class="internship-contact">
-                <h2 class="sction-title-blue">Join our team!</h2>
+                <h2 class="section-title">Join our team!</h2>
                 <p>Please contact</p>
                 <a href="<?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?>"><?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?></a>
             </section><!-- end internship contact -->
 
         <!-- third section internship why -->
             <section class="internship-why">
-                <h2 class="sction-title-blue">Why intern with us?</h2>
+                <h2 class="section-title">Why intern with us?</h2>
                 <p><?php echo CFS()->get( 'mffs_internship_description', 19 ) ?></p>
 
                 <ul class="why-grid">
@@ -34,8 +34,8 @@ get_header(); ?>
 					$loop = CFS()->get( 'mffs_internship_qualities_list', 19 );
 						foreach ( $loop as $row ) { ?>
 						<li class="internship-quality-single">
-							<img src="<?php echo $row['	internship_icon'];?>" alt="">
-                            <h2 class="section-title"><?php echo $row['mffs_internship_quality_title'];?></h2>
+							<img src="<?php echo $row['internship_icon'];?>" alt="">
+                            <h2 class="title-no-bg"><?php echo $row['mffs_internship_quality_title'];?></h2>
                             <P><?php echo $row['mffs_internship_quality_text'];?></P>
 						</li>
 						<?php } ?>
@@ -44,7 +44,7 @@ get_header(); ?>
 
             <!-- fourth section internship requirements -->
             <section class="internship-requirements">
-                <h2 class="sction-title-blue">Internship-requirements</h2>
+                <h2 class="section-title">Internship-requirements</h2>
                 <p><?php echo CFS()->get( 'mffs_internship_requirement_education', 19 ) ?></p>
 
                 <ul class="requirements-grid">
@@ -52,7 +52,7 @@ get_header(); ?>
 					$loop = CFS()->get( 'mffs_internship_requirements', 19 );
 						foreach ( $loop as $row ) { ?>
 						<li class="requirements-single">
-                            <h2 class="section-title"><?php echo $row['mffs_internship_requirement_title'];?></h2>
+                            <h2 class="title-no-bg"><?php echo $row['mffs_internship_requirement_title'];?></h2>
                             <P><?php echo $row['mffs_internship_requirement_text'];?></P>
 						</li>
 						<?php } ?>
@@ -63,7 +63,7 @@ get_header(); ?>
 
             <!-- fifth contact section -->
             <section class="internship-contact">
-                <h2 class="sction-title-blue">Join our team!</h2>
+                <h2 class="section-title">Join our team!</h2>
                 <p>How to apply</p>
                 <a href="<?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?>"><?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?></a>
             </section><!-- end internship contact -->
--- themes/mffs-theme/page-reports.php
+++ themes/mffs-theme/page-reports.php
@@ -12,24 +12,53 @@ get_header(); ?>
 
             <!-- first section annual reports -->
 			<section class="reports-info-img">
+
                 <div class="reports-info">
-                <h2 class="section-title">Annual Reports</h2>
-                <p></p>
+                    <h2 class="section-title">Annual Reports</h2>
+                    <p><?php echo CFS()->get( 'mffs_reports_info', 99 ) ?></p>
                 </div>
+
                 <div class="reports-img">
+                <ul class="reports-img-grid">
+                    <?php
+                        $loop = CFS()->get( 'reports_images', 99 );
+                            foreach ( $loop as $row ) { ?>
+                            <li class="reports-image-single">
+                                <img src="<?php echo $row['mffs_report_image_single'];?>" alt="">
+                            </li>
+                            <?php } ?>
+                    </ul>
                 </div>
             </section><!-- end reports annual -->
 
             <!-- second section latest report version -->
             <section class="reports-version-archive">
 
+                <!-- reports bottom left side, img, info, download -->
                 <div class="reports-version-latest">
-                    <h2 class="section-title">Latest Version</h2>
+                    <div>
+                        <h2 class="section-title">Latest Version</h2>
+                        <img src="<?php echo CFS()->get( 'mffs_reports_image', 99 ) ?>" alt="">
+                    </div>
+                    <div>
+                        <p><?php echo CFS()->get( 'mffs_reports_info', 99 ) ?></p>
+                        <button>Download</button>
+                    </div>
                 </div>
 
+                <!-- reports bottom right side, reports archive -->
                 <div class="reports-archive">
                     <h2 class="section-title">Archives</h2>
 
+                    <ul class="report-archive-grid">
+                    <?php
+                        $loop = CFS()->get( 'mffs_report_archive', 99 );
+                            foreach ( $loop as $row ) { ?>
+                            <li class="reports-single">
+                                <a href="<?php echo $row['mffs_archive_report_single'];?>"></a>
+                            </li>
+                            <?php } ?>
+                    </ul>
                 </div>
             </section><!-- end reports version -->
 
