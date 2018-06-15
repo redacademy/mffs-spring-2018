1866adafa24be56172164a28e28188b11eb89cf7
johnstonstu <stu.w.johnston@gmail.com>
2018-06-14 19:41:21 -0700

    header logo links to front page

--- themes/mffs-theme/header.php
+++ themes/mffs-theme/header.php
@@ -32,7 +32,7 @@
 
 				<!-- header middle logo -->
 				<div class="site-branding">
-					<a href="#"><img src="<?php echo get_template_directory_uri()?>/assets/icons/leafs.svg" alt="">
+					<a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri()?>/assets/icons/leafs.svg" alt="">
 					<p>mffs<span class="ca-tld">.ca</span></p></a>
 				</div><!-- .site-branding -->
 
