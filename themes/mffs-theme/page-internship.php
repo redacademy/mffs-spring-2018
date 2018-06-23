dbc5bfc40d9263f4d36e1b20740a0c7b668a39ce
Showwtime <jason.f.murphy@gmail.com>
2018-06-23 14:23:54 -0700

    sanitized page-internship.php

--- themes/mffs-theme/page-internship.php
+++ themes/mffs-theme/page-internship.php
@@ -15,13 +15,13 @@ get_header(); ?>
 
         <!-- first section internship title -->
             <section class="internship-title">
-                <h2 class="title-no-bg"><?php echo CFS()->get( 'mffs_internship_title', 19 ) ?></h2>
+                <h2 class="title-no-bg"><?php echo esc_html( CFS()->get( 'mffs_internship_title', 19 ) ) ?></h2>
                 <div class="intern-wrapper-desktop">
-                    <img src="<?php echo CFS()->get( 'mffs_internship_img', 19 ) ?>" alt="">
+                    <img src="<?php echo esc_url( CFS()->get( 'mffs_internship_img', 19 ) ) ?>" alt="">
                     <div class="intern-text-wrapper">
                         <h2 class="visible-desktop">Intern with us!</h2>
-                        <p><?php echo CFS()->get( 'mffs_internship_description', 19 ) ?></p>
-                        <p><?php echo CFS()->get( 'mffs_internship_text', 19 ) ?></p>
+                        <p><?php echo esc_html( CFS()->get( 'mffs_internship_description', 19 ) ) ?></p>
+                        <p><?php echo esc_html( CFS()->get( 'mffs_internship_text', 19 ) ) ?></p>
                     </div>
                 </div>
             </section> <!-- end internship title -->
@@ -30,7 +30,7 @@ get_header(); ?>
             <section class="internship-contact">
                 <h2 class="section-title">Join our team!</h2>
                 <p>Please contact</p>
-                <a href="<?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?>"><?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?></a>
+                <a href="<?php echo esc_url( CFS()->get( 'mffs_internship_contact', 19 ) ) ?>"><?php echo esc_html( CFS()->get( 'mffs_internship_contact', 19 ) ) ?></a>
             </section><!-- end internship contact -->
 
         <!-- third section internship why -->
@@ -38,16 +38,16 @@ get_header(); ?>
                 <h2 class="section-title">Why intern with us?</h2>
                 <p class="visible-desktop">Flexible non-traditional hours and a variety of activities
                 </p>
-                <p><?php echo CFS()->get( 'mffs_internship_description', 19 ) ?></p>
+                <p><?php echo CFS()->get( 'mffs_internship_description', 19 ) ?></p> <!-- TODO wp_kses-->
 
                 <ul class="why-grid">
 				<?php
 					$loop = CFS()->get( 'mffs_internship_qualities_list', 19 );
 						foreach ( $loop as $row ) { ?>
 						<li class="internship-quality-single">
-							<img src="<?php echo $row['internship_icon'];?>" alt="">
-                            <h2 class="title-no-bg"><?php echo $row['mffs_internship_quality_title'];?></h2>
-                            <?php echo $row['mffs_internship_quality_text'];?>
+							<img src="<?php echo esc_url( $row['internship_icon'] );?>" alt="">
+                            <h2 class="title-no-bg"><?php echo esc_html( $row['mffs_internship_quality_title'] );?></h2>
+                            <?php echo $row['mffs_internship_quality_text'];?> <!-- TODO wp_kses-->
 						</li>
 						<?php } ?>
 				</ul>
@@ -57,15 +57,15 @@ get_header(); ?>
             <section class="internship-requirements">
                 <h2 class="section-title">Internship-requirements</h2>
                 <h2 class="visible-desktop">Admissions Criteria<h2>
-                <p><?php echo CFS()->get( 'mffs_internship_requirement_education', 19 ) ?></p>
+                <p><?php echo esc_html( CFS()->get( 'mffs_internship_requirement_education', 19 ) ) ?></p>
 
                 <ul class="requirements-grid">
 				<?php
 					$loop = CFS()->get( 'mffs_internship_requirements', 19 );
 						foreach ( $loop as $row ) { ?>
 						<li class="requirements-single">
-                            <h2 class="title-no-bg"><?php echo $row['mffs_internship_requirement_title'];?></h2>
-                            <?php echo $row['mffs_internship_requirement_text'];?>
+                            <h2 class="title-no-bg"><?php echo esc_html( $row['mffs_internship_requirement_title'] );?></h2>
+                            <?php echo $row['mffs_internship_requirement_text'];?><!-- TODO wp_kses-->
                             <div class="yellow-dots"> &bull; &bull; &bull; </div>
                         </li>
 						<?php } ?>
@@ -78,7 +78,7 @@ get_header(); ?>
             <section class="internship-contact-end">
                 <h2 class="section-title">How to apply</h2>
                 <p>Please cntact</p>
-                <a href="<?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?>"><?php echo CFS()->get( 'mffs_internship_contact', 19 ) ?></a>
+                <a href="<?php echo esc_html( CFS()->get( 'mffs_internship_contact', 19 ) ) ?>"><?php echo esc_html( CFS()->get( 'mffs_internship_contact', 19 ) ) ?></a>
             </section><!-- end internship contact -->
                   <?php endwhile;
 			else :
